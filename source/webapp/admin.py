c9fb7e8cd275907bbd622b7e800d50d2905a3d33
Abylay Kuvatov <abukuvatov@icloud.com>
2023-02-14 02:31:06 +0600

    Добавлены поля приложения To-Do list в админку и изменен часовой пояс в файле settings.py на Asia/Almaty

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from webapp.models import ToDoList
+
+
 # Register your models here.
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'action_date']
+    list_filter = ['status', 'action_date']
+    search_fields = ['status']
+    fields = ['description', 'status', 'action_date']
+    list_editable = ['description', 'status', 'action_date']
+
+
+admin.site.register(ToDoList, ToDoListAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,23 @@
+from datetime import date
+
 from django.db import models
 
+
 # Create your models here.
+class ToDoList(models.Model):
+    NEW_STATUS = 'new'
+    IN_PROGRESS_STATUS = 'in progress'
+    DONE_STATUS = 'done'
+    STATUS_CHOICES = [
+        (NEW_STATUS, 'Новая'),
+        (IN_PROGRESS_STATUS, 'В процессе'),
+        (DONE_STATUS, 'Сделано')
+    ]
+    description = models.CharField(max_length=200, null=False, blank=False, help_text="Describe the task",
+                                   verbose_name="Description")
+    status = models.CharField(max_length=20, null=False, blank=False, choices=STATUS_CHOICES, default=NEW_STATUS,
+                              verbose_name="Status")
+    action_date = models.DateField(default=date.today, verbose_name="Date of completion")
+
+    def __str__(self):
+        return f"{self.description} - {self.status} - {self.action_date}"
