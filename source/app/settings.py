7123ec78102a4855271f0089e468f43a9492ae9a
Abylay Kuvatov <abukuvatov@icloud.com>
2023-02-23 14:45:39 +0600

    Добавил Django Debug Toolbar

--- source/app/settings.py
+++ source/app/settings.py
@@ -36,10 +36,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "webapp",
-    'bootstrap5',
+    "debug_toolbar",
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -118,3 +120,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from webapp.views.base import home_view
 from webapp.views.tasks import tasks_view, task_create, task_edit, task_details, task_delete
 
 urlpatterns = [
+    path('__debug__/', include('debug_toolbar.urls')),
     path("", home_view, name='home'),
     path('tasks', tasks_view, name='tasks_view'),
     path('task/add', task_create, name='task_create'),
