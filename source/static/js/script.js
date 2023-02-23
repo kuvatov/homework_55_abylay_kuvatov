5bc2fb31813fe34339c6272cda2bbdde8b9b4c1a
Abylay Kuvatov <abukuvatov@icloud.com>
2023-02-23 23:03:29 +0600

    Добавил js скрипт для автоматического расширения окна textarea, а также добавил checkbox-ы для задач

--- source/static/js/script.js
+++ source/static/js/script.js
@@ -1,4 +1,4 @@
-textarea = document.querySelector("#autoresizing");
+textarea = document.querySelector('textarea');
   if(textarea){
     textarea.addEventListener('input', autoResize, false);
     textarea.addEventListener('click', autoResize, false);
@@ -8,3 +8,18 @@ textarea = document.querySelector("#autoresizing");
         this.style.height = 'auto';
         this.style.height = this.scrollHeight + 'px';
     }
+
+
+function checkbox_delete() {
+  // Get the checkbox
+  var checkBox = document.getElementById("btn-checkbox");
+  // Get the output text
+  var text = document.getElementById("checkbox-button");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
